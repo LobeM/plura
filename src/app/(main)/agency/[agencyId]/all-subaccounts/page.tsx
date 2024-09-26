d316bfba362b28c5fc0072c76d5a83c9b462a5e6
Lobe <lo.musonda@pacra.org.zm>
2024-09-26 11:59:18 +0200

    added empty state

--- src/app/(main)/agency/[agencyId]/all-subaccounts/page.tsx
+++ src/app/(main)/agency/[agencyId]/all-subaccounts/page.tsx
@@ -42,64 +42,64 @@ const AllSubaccountsPage = async ({ params }: Props) => {
           <CommandList>
             <CommandEmpty>No Results Found.</CommandEmpty>
             <CommandGroup heading='Sub Accounts'>
-              {!!user.Agency?.SubAccount.length
-                ? user.Agency.SubAccount.map((subaccount: SubAccount) => (
-                    <CommandItem
-                      key={subaccount.id}
-                      className='h-32 !bg-background my-2 text-primary border border-border p-4 rounded-lg hover:!bg-background cursor-pointer transition-all'
+              {!!user.Agency?.SubAccount.length ? (
+                user.Agency.SubAccount.map((subaccount: SubAccount) => (
+                  <CommandItem
+                    key={subaccount.id}
+                    className='h-32 !bg-background my-2 text-primary border border-border p-4 rounded-lg hover:!bg-background cursor-pointer transition-all'
+                  >
+                    <Link
+                      href={`/subaccount/${subaccount.id}`}
+                      className='flex gap-4 w-full h-full'
                     >
-                      <Link
-                        href={`/subaccount/${subaccount.id}`}
-                        className='flex gap-4 w-full h-full'
-                      >
-                        <div className='relative w-32'>
-                          <Image
-                            src={subaccount.subAccountLogo}
-                            alt='Subaccount logo'
-                            fill
-                            className='rounded-md object-contain bg-muted/50 p-4'
-                          />
-                        </div>
-                        <div className='flex flex-col justify-between'>
-                          <div className='flex flex-col'>
-                            {subaccount.name}
-                            <div className='text-muted-foreground text-xs'>
-                              {subaccount.address}
-                            </div>
+                      <div className='relative w-32'>
+                        <Image
+                          src={subaccount.subAccountLogo}
+                          alt='Subaccount logo'
+                          fill
+                          className='rounded-md object-contain bg-muted/50 p-4'
+                        />
+                      </div>
+                      <div className='flex flex-col justify-between'>
+                        <div className='flex flex-col'>
+                          {subaccount.name}
+                          <div className='text-muted-foreground text-xs'>
+                            {subaccount.address}
                           </div>
                         </div>
-                      </Link>
-                      <AlertDialogTrigger asChild>
-                        <Button
-                          size='sm'
-                          variant='destructive'
-                          className='text-red-600 w-20 hover:bg-red-600 hover:text-white'
-                        >
-                          Delete
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle className='text-left'>
-                            Are you absolutely sure?
-                          </AlertDialogTitle>
-                          <AlertDialogDescription className='text-left'>
-                            This action cannot be undone. This will delete the
-                            subaccount and all data related to it.
-                          </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter className='flex items-center'>
-                          <AlertDialogCancel className='mb-2'>
-                            Cancel
-                          </AlertDialogCancel>
-                          <AlertDialogAction className='bg-destructive hover:bg-destructive'>
-                            <DeleteButton subaccountId={subaccount.id} />
-                          </AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </CommandItem>
-                  ))
-                : ''}
+                      </div>
+                    </Link>
+                    <AlertDialogTrigger asChild>
+                      <Button size='sm' variant='destructive'>
+                        Delete
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle className='text-left'>
+                          Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription className='text-left'>
+                          This action cannot be undone. This will delete the
+                          subaccount and all data related to it.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter className='flex items-center'>
+                        <AlertDialogCancel className='mb-2'>
+                          Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction className='bg-destructive hover:bg-destructive'>
+                          <DeleteButton subaccountId={subaccount.id} />
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </CommandItem>
+                ))
+              ) : (
+                <div className='text-muted-foreground text-center p-4'>
+                  No sub accounts
+                </div>
+              )}
             </CommandGroup>
           </CommandList>
         </Command>
