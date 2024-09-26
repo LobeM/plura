a7c635d425152ca9a1015be593e3b9d742a4d21e
Lobe <lo.musonda@pacra.org.zm>
2024-09-26 12:38:31 +0200

    implemented create subaccount

--- src/app/(main)/agency/[agencyId]/all-subaccounts/page.tsx
+++ src/app/(main)/agency/[agencyId]/all-subaccounts/page.tsx
@@ -24,6 +24,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import DeleteButton from './_components/delete-button';
+import CreateSubaccountButton from './_components/create-subaccount';
 
 type Props = {
   params: { agencyId: string };
@@ -36,7 +37,11 @@ const AllSubaccountsPage = async ({ params }: Props) => {
   return (
     <AlertDialog>
       <div className='flex flex-col'>
-        <Button>Create</Button>
+        <CreateSubaccountButton
+          user={user}
+          id={params.agencyId}
+          className='w-[200px] self-end m-6'
+        />
         <Command className='rounded-lg bg-transparent'>
           <CommandInput placeholder='Search Account...' />
           <CommandList>
