9597848a69e4dafdf5fbce6001fe4cb47069c789
Lobe <lobe350@gmail.com>
2024-10-23 15:21:03 +0200

    added create customer route

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -17,6 +17,7 @@ import {
 } from './queries';
 import { db } from './db';
 import { z } from 'zod';
+import Stripe from 'stripe';
 
 export type NotificationWithUser =
   | ({
@@ -125,3 +126,25 @@ export const ContactUserFormSchema = z.object({
   name: z.string().min(1, 'Required'),
   email: z.string().email(),
 });
+
+export type Address = {
+  city: string;
+  country: string;
+  line1: string;
+  postal_code: string;
+  state: string;
+};
+
+export type ShippingInfo = {
+  address: Address;
+  name: string;
+};
+
+export type StripeCustomerType = {
+  email: string;
+  name: string;
+  shipping: ShippingInfo;
+  address: Address;
+};
+
+export type PricesList = Stripe.ApiList<Stripe.Price>;
